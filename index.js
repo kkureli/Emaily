76519c97162c92999da639b91b5dd9fb65d79368
Kaan <kkureli@gmail.com>
2020-05-01 04:13:24 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+const PORT = process.env.PORT || 5000; //Whenever Heroku runs or application it has the ability to inject what are called environment variablesenvironment variables are variables that are set in the underlying runtime that node is running on top of.
+
+app.listen(PORT);
